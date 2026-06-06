111a8a41f13937b057db405b5047b90c8624bf5e
paoldev <55021433+paoldev@users.noreply.github.com>
2026-06-06 17:49:48 +0200

    Update VS2026 batch file after 'cpprestsdk' repository being archived.

--- cpprestsdk_vs2026.bat
+++ cpprestsdk_vs2026.bat
@@ -2,6 +2,21 @@
 
 setlocal
 
+set BUILD_DIR=%~dp0
+set CPPREST_DIR=%BUILD_DIR%\..\cpprestsdk
+if not exist %CPPREST_DIR% (
+echo Error: missing %CPPREST_DIR%
+goto :eof
+)
+
+set DO_BUILD=0
+set UPDATE_VCPKG=0
+
+if /i "%1"=="build" (set DO_BUILD=1)
+if /i "%2"=="build" (set DO_BUILD=1)
+if /i "%1"=="update_vcpkg" (set UPDATE_VCPKG=1)
+if /i "%2"=="update_vcpkg" (set UPDATE_VCPKG=1)
+
 set DIR_SUFFIX=-vs2026
 set VS_GENERATOR="Visual Studio 18 2026"
 set VS_VERSION=18.0
@@ -13,18 +28,29 @@ set VS_INSTALL_DIR=%%i
 
 call "%VS_INSTALL_DIR%\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-set BUILD_DIR=%~dp0
-set CPPREST_DIR=%BUILD_DIR%\..\cpprestsdk
+rem Remove trailing slash from %BUILD_DIR% and '..' from %CPPREST_DIR%
+pushd %BUILD_DIR%
+set BUILD_DIR=%cd%
+popd
+pushd %CPPREST_DIR%
+set CPPREST_DIR=%cd%
+popd
 set VCPKG_DIR=%CPPREST_DIR%\vcpkg
 set VCPKG_TOOLCHAIN=%VCPKG_DIR%\scripts\buildsystems\vcpkg.cmake
 rem Disable 'websockets' lib, since it doesn't compile with latest boost and asio libs.
-set COMMON_OPTIONS=-DCPPREST_EXCLUDE_WEBSOCKETS:BOOL=ON
+rem Add 'CMAKE_POLICY_VERSION_MINIMUM' to support latest cmake versions without changing original CMakeLists.txt.
+set COMMON_OPTIONS=-DCPPREST_EXCLUDE_WEBSOCKETS:BOOL=ON -DCMAKE_POLICY_VERSION_MINIMUM=3.10
 set STATIC_OPTIONS=-DVCPKG_TARGET_TRIPLET=x64-windows-static -DBUILD_SHARED_LIBS:BOOL=FALSE -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded$<$<CONFIG:Debug>:Debug>"
 
-if /I "%1"=="build" (
-set BUILD_CMD=cmake --build . --config Debug ^& cmake --build . --config Release
-) else (
-set BUILD_CMD=echo.
+if %UPDATE_VCPKG% equ 1 (
+echo.
+echo Update vcpkg
+pushd %CPPREST_DIR%
+git submodule update --recursive --remote --checkout -- vcpkg
+popd
+pushd %VCPKG_DIR%
+call bootstrap-vcpkg.bat -disableMetrics
+popd
 )
 
 echo.
@@ -34,18 +60,48 @@ set VCPKG_PKGS=boost-asio boost-system boost-date-time boost-regex openssl zlib
 vcpkg.exe install --vcpkg-root %VCPKG_DIR% --triplet x64-windows %VCPKG_PKGS%
 vcpkg.exe install --vcpkg-root %VCPKG_DIR% --triplet x64-windows-static %VCPKG_PKGS%
 vcpkg.exe install --vcpkg-root %VCPKG_DIR% --triplet x64-uwp %VCPKG_PKGS%
+vcpkg.exe upgrade --vcpkg-root %VCPKG_DIR% --no-dry-run
 popd
+
+rem Select 'cmake' from vcpkg, if it exists; usually, its version is greater than the one from Visual Studio.
+set CMAKE_EXE=cmake.exe
+for /f "usebackq delims=" %%i in (`findstr /C:"-windows-x86_64/bin/cmake.exe" "%VCPKG_DIR%\scripts\vcpkg-tools.json"`) do set CMAKE_PATH=%%i
+set CMAKE_PATH=%CMAKE_PATH:      "executable": "=%
+set CMAKE_PATH=%CMAKE_PATH:",=%
+set CMAKE_DIR=%CMAKE_PATH:-x86_64/bin/cmake.exe=%
+if exist "%VCPKG_DIR%\downloads\tools\%CMAKE_DIR%\%CMAKE_PATH%" (
+set CMAKE_EXE="%VCPKG_DIR%\downloads\tools\%CMAKE_DIR%\%CMAKE_PATH%"
+) else (
+if "%VCPKG_DOWNLOADS%" neq "" (
+if exist "%VCPKG_DOWNLOADS%\tools\%CMAKE_DIR%\%CMAKE_PATH%" (
+set CMAKE_EXE="%VCPKG_DOWNLOADS%\tools\%CMAKE_DIR%\%CMAKE_PATH%"
+)
+)
+)
+
+echo.
+echo Selected cmake: %CMAKE_EXE%
+echo.
+%CMAKE_EXE% --version
+
+if %DO_BUILD% equ 1 (
+set BUILD_CMD=%CMAKE_EXE% --build . --config Debug ^& %CMAKE_EXE% --build . --config Release
+) else (
+set BUILD_CMD=echo.
+)
 
 echo.
 echo Apply cpprestsdk patches from vcpkg (please ignore any error when calling this batch multiple times)
-set PATCH_DIR=%VCPKG_DIR%\ports\cpprestsdk
+set PATCH_DIR=%BUILD_DIR%\patches
 pushd %CPPREST_DIR%
 git apply %PATCH_DIR%\fix-find-openssl.patch
-git apply %PATCH_DIR%\fix_narrowing.patch
+git apply %PATCH_DIR%\fix-narrowing.patch
 git apply %PATCH_DIR%\fix-uwp.patch
 git apply %PATCH_DIR%\fix-clang-dllimport.patch
-git apply %PATCH_DIR%\silence-stdext-checked-array-iterators-warning.patch
 git apply %PATCH_DIR%\fix-asio-error.patch
+git apply %PATCH_DIR%\fix-incomplete-json-value.patch
+git apply %PATCH_DIR%\remove-stdext-checked-array-iterator-1836.patch
+git apply %PATCH_DIR%\remove-openprot-1844.diff
 popd
 
 echo.
@@ -53,7 +109,7 @@ set CONFIGURATION=winhttp-x64-windows%DIR_SUFFIX%
 echo %CONFIGURATION%
 mkdir "%BUILD_DIR%\%CONFIGURATION%"
 pushd "%BUILD_DIR%\%CONFIGURATION%"
-cmake -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% "%CPPREST_DIR%\Release"
+%CMAKE_EXE% -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% "%CPPREST_DIR%\Release"
 %BUILD_CMD%
 popd
 
@@ -62,7 +118,7 @@ set CONFIGURATION=winhttp-x64-windows-static%DIR_SUFFIX%
 echo %CONFIGURATION%
 mkdir "%BUILD_DIR%\%CONFIGURATION%"
 pushd "%BUILD_DIR%\%CONFIGURATION%"
-cmake -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% %STATIC_OPTIONS% "%CPPREST_DIR%\Release"
+%CMAKE_EXE% -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% %STATIC_OPTIONS% "%CPPREST_DIR%\Release"
 %BUILD_CMD%
 popd
 
@@ -72,7 +128,7 @@ set CONFIGURATION=winhttp-x64-windows-narrow%DIR_SUFFIX%
 echo %CONFIGURATION%
 mkdir "%BUILD_DIR%\%CONFIGURATION%"
 pushd "%BUILD_DIR%\%CONFIGURATION%"
-cmake -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% -DCPPREST_FORCE_NARROW_STRINGS:BOOL=TRUE "%CPPREST_DIR%\Release"
+%CMAKE_EXE% -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% -DCPPREST_FORCE_NARROW_STRINGS:BOOL=TRUE "%CPPREST_DIR%\Release"
 %BUILD_CMD%
 popd
 
@@ -82,7 +138,7 @@ set CONFIGURATION=winhttp-x64-windows-static-narrow%DIR_SUFFIX%
 echo %CONFIGURATION%
 mkdir "%BUILD_DIR%\%CONFIGURATION%"
 pushd "%BUILD_DIR%\%CONFIGURATION%"
-cmake -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% %STATIC_OPTIONS% -DCPPREST_FORCE_NARROW_STRINGS:BOOL=TRUE "%CPPREST_DIR%\Release"
+%CMAKE_EXE% -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% %STATIC_OPTIONS% -DCPPREST_FORCE_NARROW_STRINGS:BOOL=TRUE "%CPPREST_DIR%\Release"
 %BUILD_CMD%
 popd
 
@@ -92,7 +148,7 @@ set CONFIGURATION=asio-x64-windows%DIR_SUFFIX%
 echo %CONFIGURATION%
 mkdir "%BUILD_DIR%\%CONFIGURATION%"
 pushd "%BUILD_DIR%\%CONFIGURATION%"
-cmake -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% -DCPPREST_HTTP_CLIENT_IMPL=asio -DCPPREST_HTTP_LISTENER_IMPL=asio -DCMAKE_CXX_FLAGS="/DCPPREST_FORCE_HTTP_CLIENT_ASIO /EHsc" "%CPPREST_DIR%\Release"
+%CMAKE_EXE% -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% -DCPPREST_HTTP_CLIENT_IMPL=asio -DCPPREST_HTTP_LISTENER_IMPL=asio -DCMAKE_CXX_FLAGS="/DCPPREST_FORCE_HTTP_CLIENT_ASIO /EHsc" "%CPPREST_DIR%\Release"
 %BUILD_CMD%
 popd
 
@@ -102,18 +158,18 @@ set CONFIGURATION=asio-x64-windows-static%DIR_SUFFIX%
 echo %CONFIGURATION%
 mkdir "%BUILD_DIR%\%CONFIGURATION%"
 pushd "%BUILD_DIR%\%CONFIGURATION%"
-cmake -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% %STATIC_OPTIONS% -DCPPREST_HTTP_CLIENT_IMPL=asio -DCPPREST_HTTP_LISTENER_IMPL=asio -DCMAKE_CXX_FLAGS="/DCPPREST_FORCE_HTTP_CLIENT_ASIO /EHsc" "%CPPREST_DIR%\Release"
+%CMAKE_EXE% -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% %STATIC_OPTIONS% -DCPPREST_HTTP_CLIENT_IMPL=asio -DCPPREST_HTTP_LISTENER_IMPL=asio -DCMAKE_CXX_FLAGS="/DCPPREST_FORCE_HTTP_CLIENT_ASIO /EHsc" "%CPPREST_DIR%\Release"
 %BUILD_CMD%
 popd
 
 rem This configuration requires "Microsoft.VisualStudio.Component.Windows10SDK" (e.g. 10.0.19041.0).
 rem If only Windows11SDK is installed (>= 10.0.22000.0, Microsoft.VisualStudio.Component.Windows11Sdk),
-rem at least cmake > 4.2.1 is required (not yet available).
+rem at least cmake >= 4.3.0 is required.
 echo.
 set CONFIGURATION=winrt-x64-uwp%DIR_SUFFIX%
 echo %CONFIGURATION%
 mkdir "%BUILD_DIR%\%CONFIGURATION%"
 pushd "%BUILD_DIR%\%CONFIGURATION%"
-cmake -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 "%CPPREST_DIR%\Release"
+%CMAKE_EXE% -G %VS_GENERATOR% -A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_TOOLCHAIN%" %COMMON_OPTIONS% -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 "%CPPREST_DIR%\Release"
 %BUILD_CMD%
 popd
